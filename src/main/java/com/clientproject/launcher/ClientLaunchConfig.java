68482a002af125bf95c78af89b7152dccdbf3700
Xemip <115277536+Xemip@users.noreply.github.com>
2026-03-10 22:48:08 +0530

    Update ClientLaunchConfig.java

--- src/main/java/com/clientproject/launcher/ClientLaunchConfig.java
+++ src/main/java/com/clientproject/launcher/ClientLaunchConfig.java
@@ -1,44 +1,20 @@
-package com.clientproject.client.visual;
+package com.clientproject.launcher;
 
-import com.clientproject.client.modules.BaseModule;
-import com.clientproject.client.modules.ModuleCategory;
+public class ClientLaunchConfig {
 
-public final class OptiFineUltraModule extends BaseModule {
-    private static final int MIN_RENDER_DISTANCE = 2;
-    private static final int MAX_RENDER_DISTANCE = 64;
+    private String gameDir;
+    private String version;
 
-    private boolean shaders = false;
-    private boolean zoom = true;
-    private int renderDistanceChunks = 8;
-
-    public OptiFineUltraModule() {
-        super("optifine_ultra", "OptiFine Ultra", ModuleCategory.VISUAL, true);
+    public ClientLaunchConfig(String gameDir, String version) {
+        this.gameDir = gameDir;
+        this.version = version;
     }
 
-    public boolean shaders() {
-        return shaders;
+    public String getGameDir() {
+        return gameDir;
     }
 
-    public void setShaders(boolean shaders) {
-        this.shaders = shaders;
-    }
-
-    public boolean zoom() {
-        return zoom;
-    }
-
-    public void setZoom(boolean zoom) {
-        this.zoom = zoom;
-    }
-
-    public int renderDistanceChunks() {
-        return renderDistanceChunks;
-    }
-
-    public void setRenderDistanceChunks(int renderDistanceChunks) {
-        if (renderDistanceChunks < MIN_RENDER_DISTANCE || renderDistanceChunks > MAX_RENDER_DISTANCE) {
-            throw new IllegalArgumentException("renderDistanceChunks out of range");
-        }
-        this.renderDistanceChunks = renderDistanceChunks;
+    public String getVersion() {
+        return version;
     }
 }
