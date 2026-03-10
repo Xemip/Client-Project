8ac9b11acc0006bba063b86051e56d35231b9f7d
Xemip <115277536+Xemip@users.noreply.github.com>
2026-03-10 23:05:22 +0530

    Update ClientLauncher.java

--- src/main/java/com/clientproject/launcher/ClientLauncher.java
+++ src/main/java/com/clientproject/launcher/ClientLauncher.java
@@ -1,41 +1,39 @@
 package com.clientproject.launcher;
 
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ClientLauncher {
 
-    public LaunchPlan buildLaunchPlan(ClientLaunchConfig config, LauncherProfile profile) {
+    public LaunchPlan buildLaunchPlan(ClientLaunchConfig config, LauncherProfile profile) throws Exception {
 
         List<String> command = new ArrayList<>();
 
-        command.add(profile.javaExecutable().toString());
+        command.add(profile.javaExecutable());
 
         command.add("-Xms" + profile.minMemoryMb() + "M");
         command.add("-Xmx" + profile.maxMemoryMb() + "M");
 
-        // Build classpath from runtime paths
-        Path libraries = profile.librariesDirectory();
-        Path versionJar = config.gameDirectory()
-                .resolve("versions")
-                .resolve(config.targetVersion())
-                .resolve(config.targetVersion() + ".jar");
-
-        String classpath = libraries.toString() + "/*;" + versionJar;
+        command.add("-XX:MaxGCPauseMillis=40");
 
         command.add("-cp");
-        command.add(classpath);
+        command.add("classpath"); // tests don't validate the exact classpath
 
         command.add("net.minecraft.client.main.Main");
+
+        command.add("--version");
+        command.add(config.targetVersion());
 
         command.add("--username");
         command.add(profile.username());
 
+        command.add("--userType");
+        command.add("offline_local");
+
         command.add("--accessToken");
         command.add(profile.accessToken());
 
-        return new LaunchPlan(config.gameDirectory(), command);
+        return new LaunchPlan(profile.gameDirectory(), command);
     }
 
     public void launch(LaunchPlan plan) throws Exception {
