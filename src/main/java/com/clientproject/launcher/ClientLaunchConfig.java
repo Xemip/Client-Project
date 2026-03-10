2f9092eae03d1584c3edb101bffc4cc18bf9644e
Xemip <115277536+Xemip@users.noreply.github.com>
2026-03-10 22:51:26 +0530

    Update ClientLaunchConfig.java

--- src/main/java/com/clientproject/launcher/ClientLaunchConfig.java
+++ src/main/java/com/clientproject/launcher/ClientLaunchConfig.java
@@ -2,19 +2,23 @@ package com.clientproject.launcher;
 
 public class ClientLaunchConfig {
 
-    private String gameDir;
-    private String version;
+    private final LauncherProfile profile;
+    private final String gameDir;
 
-    public ClientLaunchConfig(String gameDir, String version) {
+    public ClientLaunchConfig(LauncherProfile profile, String gameDir) {
+        this.profile = profile;
         this.gameDir = gameDir;
-        this.version = version;
+    }
+
+    public static ClientLaunchConfig fromProfile(LauncherProfile profile, String gameDir) {
+        return new ClientLaunchConfig(profile, gameDir);
+    }
+
+    public LauncherProfile getProfile() {
+        return profile;
     }
 
     public String getGameDir() {
         return gameDir;
-    }
-
-    public String getVersion() {
-        return version;
     }
 }
