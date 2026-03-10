e554d0ee99d80c40072eaa6e91d770ae6c3da9e7
Xemip <115277536+Xemip@users.noreply.github.com>
2026-03-10 22:51:37 +0530

    Update ClientLauncher.java

--- src/main/java/com/clientproject/launcher/ClientLauncher.java
+++ src/main/java/com/clientproject/launcher/ClientLauncher.java
@@ -2,8 +2,8 @@ package com.clientproject.launcher;
 
 public class ClientLauncher {
 
-    public void launch(ClientLaunchConfig config) {
-        System.out.println("Launching client version " + config.getVersion());
-        System.out.println("Game directory: " + config.getGameDir());
+    public LaunchPlan buildLaunchPlan(ClientLaunchConfig config, LauncherProfile profile) {
+        return new LaunchPlan(config, profile);
     }
+
 }
