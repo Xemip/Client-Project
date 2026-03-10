4814e532c6fa0388c60033365d6bb6c0378a53ec
Xemip <115277536+Xemip@users.noreply.github.com>
2026-03-10 22:53:17 +0530

    Update ClientLaunchConfig.java

--- src/main/java/com/clientproject/launcher/ClientLaunchConfig.java
+++ src/main/java/com/clientproject/launcher/ClientLaunchConfig.java
@@ -1,24 +1,27 @@
 package com.clientproject.launcher;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class ClientLaunchConfig {
 
     private final LauncherProfile profile;
-    private final String gameDir;
+    private final Path gameDir;
 
-    public ClientLaunchConfig(LauncherProfile profile, String gameDir) {
+    public ClientLaunchConfig(LauncherProfile profile, Path gameDir) {
         this.profile = profile;
         this.gameDir = gameDir;
     }
 
     public static ClientLaunchConfig fromProfile(LauncherProfile profile, String gameDir) {
-        return new ClientLaunchConfig(profile, gameDir);
+        return new ClientLaunchConfig(profile, Paths.get(gameDir));
     }
 
     public LauncherProfile getProfile() {
         return profile;
     }
 
-    public String getGameDir() {
+    public Path getGameDir() {
         return gameDir;
     }
 }
